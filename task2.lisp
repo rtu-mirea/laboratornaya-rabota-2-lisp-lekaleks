692f1763ada6416bb498c1559f43418f169ffecb
lekaleks <55615148+lekaleks@users.noreply.github.com>
2019-12-28 14:11:17 +0300

    Update task2.lisp
    поправил 2.2

--- task2.lisp
+++ task2.lisp
@@ -1,5 +1,17 @@
-(let ((in (open "task2.txt" :if-does-not-exist nil)))
-      (when in
-            (loop for line = (read-line in nil)
-            while line do (format t "~a~%" line))
-      (close in)))
+(defun _print_text (file)
+    (if file
+        (or
+            (loop for line = (read-line file nil)
+                while line do (format t "~a~%" line)
+            )
+            (close file)
+        )
+        (format t "~a~%" "[!] Error while opening file")
+    )
+)
+(defun print_text (path)
+    (_print_text (open path :if-does-not-exist nil))
+)
+
+(print_text "error")
+(print_text "text2.txt")
